979dbbf7445dfd23084a9c93311f8e5713c70f38
OthmanSmaili <66753081+OthmanSmaili@users.noreply.github.com>
2024-11-19 23:36:03 -0800

    Created New React App

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import 'tachyons'
+import Card from './Card.jsx'
+import { robots } from './robots.js'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+  </StrictMode>,
+)
