6f81633bfa7900347532809e4ec2e859efbbb4cc
OthmanSmaili <66753081+OthmanSmaili@users.noreply.github.com>
2024-11-19 23:47:18 -0800

    slight edit

--- src/main.jsx
+++ src/main.jsx
@@ -11,5 +11,5 @@ createRoot(document.getElementById('root')).render(
     <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
     <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
     <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-  </StrictMode>,
+  </StrictMode>
 )
